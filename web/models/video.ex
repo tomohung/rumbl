88f81e3c5b5e9e887267edbe1ca9b8a09b3905d3
Tomo Hung <tomohung@gmail.com>
2016-06-14 22:05:09 +0800

    add slug

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,12 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   schema "vidoes" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -41,4 +43,10 @@ defmodule Rumbl.Video do
     |> String.replace(~r/[^\w-]+/u, "-")
   end
 
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
+  end
 end
